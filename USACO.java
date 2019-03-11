1e77f50a2791f504461b1c3704c9a555ae98b899
Ali Taha <alitaha.1187@gmail.com>
2019-03-11 02:00:33 -0400

    finished bronze, merged all helper methods into one large method for convenience, and tested with all cases attatched in zip file works properly.

--- USACO.java
+++ USACO.java
@@ -4,90 +4,69 @@ import java.util.Scanner;
 import java.util.Arrays;
 //imorted necessary libraries so far
 public class USACO{
-  private int[] ln1;
-  private int elevation;
-  private int[][] lake;
-  private int[][] instructions;
-  public USACO(String f) throws FileNotFoundException{
-    read(f);
-  }
-  private void read(String Filename) throws FileNotFoundException{
-    File LakeData = new File(Filename);
+  public static int bronze(String filename) throws FileNotFoundException{
+    File LakeData = new File(filename);
     Scanner scanner = new Scanner(LakeData);
     //read first line and saving elevation;
-    ln1 = new int[4];
+    int[] ln1 = new int[4];
     for(int i = 0; i<ln1.length; i++){
       ln1[i]=scanner.nextInt();}
-    elevation = ln1[2];
+    int elevation = ln1[2];
     //reading lake matrix;
-    lake = new int[ln1[0]][ln1[1]];
+    int[][] lake = new int[ln1[0]][ln1[1]];
     for(int row = 0; row<lake.length;row++){
       for(int col =0; col<lake[0].length;col++){
         lake[row][col]=scanner.nextInt();
       }
     }
     //reading instructions;
-    instructions = new int[ln1[3]][3];
+    int[][] instructions = new int[ln1[3]][3];
     for(int inst = 0; inst<instructions.length;inst++){
       instructions[inst][0]=scanner.nextInt();
       instructions[inst][1]=scanner.nextInt();
       instructions[inst][2]=scanner.nextInt();
     }
-  }
-  private void operate(int[] directions){
-    int r =directions[0]-1;
-    int c =directions[1]-1;
-    //creating patch of 9x9 plot for cows to stomp on
-    int[][] patch = new int[][]{
-      {r+0,c+0},
-      {r+1,c+0},
-      {r+2,c+0},
-      {r+0,c+1},
-      {r+1,c+1},
-      {r+2,c+1},
-      {r+0,c+2},
-      {r+1,c+2},
-      {r+2,c+2}
-    };
-    int highest = lake[r][c];
-    for(int[] i : patch){
-      if(lake[i[0]][i[1]]>=highest){
-        highest = lake[i[0]][i[1]];
-        r = i[0];
-        c = i[1];
+    for(int[] g :instructions){
+      int r =g[0]-1;
+      int c =g[1]-1;
+      //creating patch of 9x9 plot for cows to stomp on
+      int[][] patch = new int[][]{
+        {r+0,c+0},
+        {r+1,c+0},
+        {r+2,c+0},
+        {r+0,c+1},
+        {r+1,c+1},
+        {r+2,c+1},
+        {r+0,c+2},
+        {r+1,c+2},
+        {r+2,c+2}
+      };
+      int highest = lake[r][c];
+      for(int[] i : patch){
+        if(lake[i[0]][i[1]]>=highest){
+          highest = lake[i[0]][i[1]];
+          r = i[0];
+          c = i[1];
+        }
+      }
+      lake[r][c]-=g[2];
+      for(int[] i:patch){
+        if(lake[i[0]][i[1]]>lake[r][c]){
+          lake[i[0]][i[1]] = lake[r][c];
+        }
       }
     }
-    lake[r][c]-=directions[2];
-    for(int[] i:patch){
-      if(lake[i[0]][i[1]]>lake[r][c]){
-        lake[i[0]][i[1]] = lake[r][c];
+    int depth =0;
+    for(int i=0; i<lake.length;i++){
+      for(int j=0;j<lake[0].length;j++){
+        if(elevation-lake[i][j]>0){
+          depth+=elevation-lake[i][j];
+        }
       }
     }
-  }
-  public static String printthis(int[][] board){
-    String brd="";
-      for(int i=0;i<board.length;i++){
-        for(int j=0;j<board[0].length;j++){
-          if(board[i][j]<10)brd+=" ";
-            if(board[i][j]==0)brd+="_";
-            else brd+=board[i][j];
-           brd+=" ";
-          }
-        brd = brd.substring(0, brd.length() - 1);
-        brd+="\n\n";
-      }
-    return brd;
+
+    return depth*72*72;
     }
   public static void main(String[] args) throws FileNotFoundException {
-    USACO test = new USACO("testFile.txt");
-    System.out.println("\nSuccessfuly Read and Stored File \nHere's the First Line:");
-    System.out.print(Arrays.toString(test.ln1));
-    System.out.println("\nHere's the Lake:");
-    System.out.print(printthis(test.lake));
-    System.out.println("\nAnd Here are the Instructions:");
-    System.out.print(printthis(test.instructions));
-    test.operate(test.instructions[0]);
-    System.out.println("\nJust Finished Stomping with given directions");
-    System.out.print(printthis(test.lake));
   }
 }
