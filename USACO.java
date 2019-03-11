d86882fdeca700c15f735851aaab976a91401d29
Ali Taha <alitaha.1187@gmail.com>
2019-03-11 00:01:57 -0400

    first commit

---
+++ USACO.java
@@ -0,0 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+//imorted necessary libraries so far
+public class USACO{
+  public static void main(String[] args) {
+    System.out.println("Created File!");
+  }
+}
